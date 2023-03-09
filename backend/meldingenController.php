8715418f1712cf0e703ac0a9178508da51246193
adiKPD <adi.benh9@gmail.com>
2023-03-09 14:46:56 +0100

    code voor W1 opdracht
    de code werkend gemaakt voor de database opdracht W1

--- backend/meldingenController.php
+++ backend/meldingenController.php
@@ -1,17 +1,40 @@
 <?php
 
-//Variabelen vullen
 $attractie = $_POST['attractie'];
 $capaciteit = $_POST['capaciteit']; 
 $melder = $_POST['melder'];
+$type = $_POST["type"];
+$overig = $_POST['overig'];
+if(isset($_POST['prioriteit']))
+{
+    $prioriteit = true;
+}
+else
+{
+    $prioriteit = false;
+}
 
-echo $attractie . " / " . $capaciteit . " / " . $melder;
 
-//1. Verbinding
+echo $attractie . " / " . $capaciteit . " / " . $melder . " / " . $type . " / " . $overig . " / " . $prioriteit;
+
 require_once 'conn.php';
 
-//2. Query
+$query = "INSERT INTO meldingen (attractie, capaciteit, melder, prioriteit, overige_info, type)
+VALUES(:attractie, :capaciteit, :melder, :prioriteit, :overige_info, :type)";
 
-//3. Prepare
+$statement = $conn->prepare($query);
 
-//4. Execute
+$statement->execute([
+    ":attractie" => $attractie,
+    ":capaciteit" => $capaciteit,
+    ":melder" => $melder,
+    ":type" => $type,
+    ":prioriteit" => $prioriteit,
+    ":overige_info" => $overig
+]);
+
+$items = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+header("lacation: storingapp/meldingen/index.php?msg= melding opgeslagen");
+
+?>
--- meldingen/create.php
+++ meldingen/create.php
@@ -19,18 +19,43 @@
                 <label for="attractie">Naam attractie:</label>
                 <input type="text" name="attractie" id="attractie" class="form-input">
             </div>
+
             <div class="form-group">
                 <label for="type">Type</label>
-                <!-- hier komt een dropdown -->
+                <select name="type" id="type">
+                    <option value=""> -kies een type - <option>
+                    <option value="A">achtbaan</option>
+                    <option value="B">draaiend</option>
+                    <option value="C">kinder</option>
+                    <option value="D">horeca</option>
+                    <option value="E">show</option>
+                    <option value="F">water</option>
+                    <option value="G">overig</option>
+                </select>
             </div>
+
             <div class="form-group">
                 <label for="capaciteit">Capaciteit p/uur:</label>
                 <input type="number" min="0" name="capaciteit" id="capaciteit" class="form-input">
             </div>
+
+            <div class="form-group">
+                <label for="prioriteit">prioriteit</label>
+                <input type="checkbox" id="prioriteit" name="prioriteit">
+                <label for="prioriteit">Melding met prioriteit</label>
+            </div>
+
             <div class="form-group">
                 <label for="melder">Naam melder:</label>
                 <input type="text" name="melder" id="melder" class="form-input">
             </div>
+
+            <div class="form-group">
+                <label for="overig">overige info</label>
+                <textarea name="overig" id="overig" class="form-input" rows="4"></textarea>
+            </div>
+
+            
             
             <input type="submit" value="Verstuur melding">
 
